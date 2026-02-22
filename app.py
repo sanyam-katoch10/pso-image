d5ddec3084cb9ac19decf5b9f55ea8d46d55f301
Your Name <you@example.com>
2026-02-22 19:15:15 +0530

    fix: rename variable and force re-indentation to fix railway error

--- app.py
+++ app.py
@@ -205,5 +205,5 @@ if __name__ == "__main__":
             except OSError:
                 pass
 
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    server_port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=server_port, debug=False)
